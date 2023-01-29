82480711f7a6d28900f69ed69a6dedaec3f3156e
Tetre101 <basilpeter989@gmail.com>
2023-01-29 11:50:00 +0100

    initialised fighter

---
+++ fighter.js
@@ -0,0 +1,9 @@
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+}
