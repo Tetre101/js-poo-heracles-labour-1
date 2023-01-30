7684f7ae27c3199a82aabb8ef0541725af79a662
Tetre101 <basilpeter989@gmail.com>
2023-01-30 02:59:17 +0100

    completed still running through an endless loop

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ const nemeanLion = new Fighter("🦁 Nemean Lion", 11, 13);
 
 console.log(`${heracles.name} has ${heracles.life} life`);
 console.log(`${nemeanLion.name} has ${nemeanLion.life} life`);
+
+let round = 1;
+while (heracles.isAlive() && nemeanLion.isAlive()) {
+  console.log(`Round ${round}:`);
+  console.log(`${heracles.name} attacks ${nemeanLion.name}`);
+  heracles.fight(nemeanLion);
+  console.log(`${nemeanLion.name} has ${nemeanLion.life} life`);
+  console.log(`${nemeanLion.name} attacks ${heracles.name}`);
+  nemeanLion.fight(heracles);
+  console.log(`${heracles.name} has ${heracles.life} life`);
+
+  round++;
+}
+
+if (!heracles.isAlive()) {
+  console.log(`${nemeanLion.name} wins 🏆`);
+  console.log(`${heracles.name} loses 💀`);
+} else {
+  console.log(`${heracles.name} wins 🏆`);
+  console.log(`${nemeanLion.name} loses 💀`);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -7,5 +7,16 @@ class Fighter {
     this.dexterity = dexterity;
     this.life = MAX_LIFE;
   }
+  fight(opponent) {
+    let damage = Math.floor(Math.random() * this.strenght) + 1;
+    let defense = opponent.dexterity;
+    let damageInflicted = damage - defense >= 0 ? damage - defense : 0;
+    opponent.life -= damageInflicted;
+    opponent.life = opponent.life < 0 ? 0 : opponent.life;
+  }
+
+  isAlive() {
+    return this.life > 0;
+  }
 }
 module.exports = Fighter;
