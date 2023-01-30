2945bfbf50a76cc16a63c26648d6bfd3da17960b
Tetre101 <basilpeter989@gmail.com>
2023-01-30 02:38:29 +0100

    added index.js and fixed fighter

--- fighter.js
+++
@@ -1,9 +0,0 @@
-const MAX_LIFE = 100;
-class Fighter {
-  constructor(name, strength, dexterity) {
-    this.name = name;
-    this.strength = strength;
-    this.dexterity = dexterity;
-    this.life = MAX_LIFE;
-  }
-}
